04b3a5874a5af0ceff67ccd0a9d2d0a64dffa510
Kyle Liu <kylesliu@outlook.com>
2019-06-25 17:36:32 +0800

    修改包名

--- metrics_test.go
+++ metrics_test.go
@@ -1,4 +1,4 @@
-package gin_metrics
+package ginMetrics
 
 import (
 	"fmt"
--- middleware.go
+++ middleware.go
@@ -1,6 +1,5 @@
-package gin_metrics
+package ginMetrics
 
-import "C"
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
